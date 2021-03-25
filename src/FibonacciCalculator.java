67454016cb1f717a1931b6011b503c2bca14109d
Piotr <pgbercik@gmail.com>
2021-03-25 23:10:28 +0100

    Showing results in scientific notation.The code ws reformatted.

--- src/FibonacciCalculator.java
+++ src/FibonacciCalculator.java
@@ -4,24 +4,27 @@ import java.util.List;
 
 public class FibonacciCalculator {
     List<BigInteger> resList = new ArrayList<>();
-    BigInteger result=new BigInteger("0");
+    BigInteger result = new BigInteger("0");
     BigInteger minus1 = null;
     BigInteger minus2 = null;
 
+    /**
+     * This method implements a simple algorithm for calculating subsequent Fibonacci numbers.
+     * More details can be found in this link:
+     * https://en.wikipedia.org/wiki/Fibonacci_number
+     */
     public BigInteger calculateFibonacci(int initNumber) {
 
-        if (initNumber==0) {
-            result=result.add(new BigInteger("0"));
+        if (initNumber == 0) {
+            result = result.add(new BigInteger("0"));
             resList.add(result);
-        }
-        else if (initNumber==1) {
-            result=result.add(new BigInteger("1"));
+        } else if (initNumber == 1) {
+            result = result.add(new BigInteger("1"));
             resList.add(result);
-        }
-        else {
-            minus1=resList.get(initNumber-1);
-            minus2=resList.get(initNumber-2);
-            result=minus1.add(minus2);
+        } else {
+            minus1 = resList.get(initNumber - 1);
+            minus2 = resList.get(initNumber - 2);
+            result = minus1.add(minus2);
 
             resList.add(result);
         }
--- src/RunFibonacci.java
+++ src/RunFibonacci.java
@@ -1,8 +1,20 @@
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class RunFibonacci {
     public static void main(String[] args) {
         FibonacciCalculator f = new FibonacciCalculator();
         for (int i = 0; i <= 399; i++) {
-            System.out.println("F(" + i + ")  wynik: " + f.calculateFibonacci(i));
+
+            //for results in scientific notation
+            NumberFormat formatter = new DecimalFormat("0.####E0", DecimalFormatSymbols.getInstance(Locale.ROOT));
+            System.out.println("F(" + i + ")  wynik: " + formatter.format(f.calculateFibonacci(i)));
+
+            //for raw results
+//            System.out.println("F(" + i + ")  wynik: " + f.calculateFibonacci(i));
+
         }
     }
 }
